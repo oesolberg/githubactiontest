424bc6d1c91d6504c9c0434c7841a747bd24b108
Odd Erik Midthaug <oesolberg@hotmail.com>
2020-04-10 09:45:11 +0200

    Create SharedAssemblyInfo.cs

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Guahtdim Software")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Guahtdim Software")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.4.8.1")]
+[assembly: AssemblyFileVersion("0.4.8.1")]
